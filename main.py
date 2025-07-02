b25a8e6f6f92d8479ab9a756a82508deb3b1a84b
Anshika Sharma <succes20s23@gmail.com>
2025-07-02 15:41:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 sns.set_style("whitegrid")  
-plt.rcParams.update({'figure.autolayout': True})  # Better spacing
+plt.rcParams.update({'figure.autolayout': True})  
 
 # ------------------------------
 # Load and Clean Data
